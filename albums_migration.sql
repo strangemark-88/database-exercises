10569edfea2daaac52fc0377cf7ce11d815d92d6
Mark Strange <strange.mark88@gmail.com>
2019-10-09 16:49:38 -0500

    albums_migration

--- albums_migration.sql
+++ albums_migration.sql
@@ -12,10 +12,10 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 
-    artist VARCHAR(50),
-    name VARCHAR(50),
-    release_date INT,
-    sales FLOAT,
-    genre varchar(50),
+    artist VARCHAR(250) NOT NULL,
+    name VARCHAR(250) NOT NULL,
+    release_date INT(4) UNSIGNED,
+    sales FLOAT(5,2) UNSIGNED,
+    genre varchar(250) NOT NULL,
     PRIMARY KEY (id)
 );
