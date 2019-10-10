aa97d3d243195cf5412ee45416faf5af4374190f
Mark Strange <strange.mark88@gmail.com>
2019-10-10 14:14:12 -0500

    add albums_seeder

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,13 +2,6 @@ USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
 
--- id — auto-incrementing unsigned integer primary key
--- artist — string for storing the recording artist name
--- name — string for storing a record name
--- release_date — integer representing year record was released
--- sales — floating point value for number of records sold (in millions)
--- genre — string for storing the record's genre(s)
-
 CREATE TABLE albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 
---
+++ select_exercises.sql
@@ -0,0 +1,15 @@
+USE codeup_test_db;
+
+-- In select_exercises.sql write queries to find the following information. Before each item, output a caption explaining the results:
+
+-- The name of all albums by Pink Floyd.
+
+-- The year Sgt. Pepper's Lonely Hearts Club Band was released
+
+-- The genre for Nevermind
+
+-- Which albums were released in the 1990s
+
+-- Which albums had less than 20 million certified sales
+
+-- All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
